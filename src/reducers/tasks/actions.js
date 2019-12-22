e2b1ebed15493942bd43fd8a3c77426b143ef54c
p-miszczuk <piotr.miszczuk@interia.pl>
2019-12-22 17:32:19 +0100

    add actions to reducer

--- src/reducers/tasks/actions.js
+++ src/reducers/tasks/actions.js
@@ -3,3 +3,28 @@ export const DELETE_LIST = "DELETE_LIST";
 export const ADD_TASK = "ADD_TASK";
 export const DELETE_TASK = "DELETE_TASK";
 export const UPDATE_TASK = "UPDATE_TASK";
+
+export const addList = payload => ({
+  type: ADD_LIST,
+  payload
+});
+
+export const deleteList = payload => ({
+  type: DELETE_LIST,
+  payload
+});
+
+export const addTask = payload => ({
+  type: ADD_TASK,
+  payload
+});
+
+export const deleteTask = payload => ({
+  type: DELETE_TASK,
+  payload
+});
+
+export const updateTask = payload => ({
+  type: UPDATE_TASK,
+  payload
+});
