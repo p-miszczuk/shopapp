dcc02f5a7ef0ccd9564be14f83b52a2b962e2682
p-miszczuk <piotr.miszczuk@reactpoland.com>
2020-05-06 22:50:58 +0200

    add extra char to date

--- src/layouts/List.js
+++ src/layouts/List.js
@@ -27,8 +27,8 @@ const ListItems = ({
     }
     const date = new Date(values.seconds * 1000);
     const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
     return `${day} / ${month} / ${year}`;
   };
 
